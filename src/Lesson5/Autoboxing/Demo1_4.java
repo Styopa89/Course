9539362fbd9e368f09f1a778a076a015d6ff4729
Styopa89 <styopa.v@gmail.com>
2016-02-23 00:29:28 +0200

    New changelist

--- src/Lesson5/Autoboxing/Demo1_4.java
+++ src/Lesson5/Autoboxing/Demo1_4.java
@@ -5,14 +5,14 @@ import java.lang.Integer;
 
 public class Demo1_4 {
     public static void main(String[] args) {
-        Integer a = 10;
+        int a = (new Integer(10)).intValue();
         Double b = new Double(12);
         int c = 5;
 
-        System.out.println(sum(a, b));
+        System.out.println(sum(a,c));
     }
 
-    public int sum(int a, int b){
+    public static int sum(int a, int b){
         return a + b;
     }
 }
