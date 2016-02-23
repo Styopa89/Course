7b7930b7277525ad7e53b679fafc6336d1d20369
styopa89 <styopa.v@gmail.com>
2016-02-23 18:03:05 +0200

    Java

---
+++ src/Lesson5/HW5Frame3/SalesJornal.java
@@ -0,0 +1,12 @@
+package Lesson5.HW5Frame3;
+
+
+public class SalesJornal {
+        private Car car;
+        private Client client;
+        private long data;
+
+
+    public SalesJornal() {
+           }
+}
