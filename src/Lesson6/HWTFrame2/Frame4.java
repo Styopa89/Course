505377146d169c943e59dad758e3fabfe7d6b70c
styopa89 <styopa.v@gmail.com>
2016-03-01 18:43:21 +0200

    Java

---
+++ src/Lesson6/HWTFrame2/Frame4.java
@@ -0,0 +1,18 @@
+package Lesson6.HWTFrame2;
+
+
+public class Frame4 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("try");
+            throw new Exception();
+
+        } catch (Exception e) {
+            System.out.println("catch");
+            throw new IllegalStateException();
+
+        } finally {
+            System.out.println("I want to be printed"); // is actually printed as well as ISE
+        }
+    }
+}
