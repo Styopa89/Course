81c4b67a53bb16c02de75b2816aa7ad604f9dbfd
styopa89 <styopa.v@gmail.com>
2016-03-16 20:10:28 +0200

    Java

--- src/lesson8/createwindow/CreateWindow.java
+++ src/lesson8/createwindow/CreateWindow.java
@@ -2,12 +2,14 @@ package lesson8.createwindow;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CreateWindow {
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
         f.setBounds(300,100, 800, 600);
+        f.setBackground(new Color(180,180,180));
         f.setVisible(true);
     }
 }
