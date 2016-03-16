fd51204729a380b30aad4fcbf42d3ca041e70486
styopa89 <styopa.v@gmail.com>
2016-03-16 20:18:49 +0200

    Java

--- src/lesson8/createwindow/CreateWindow.java
+++ src/lesson8/createwindow/CreateWindow.java
@@ -2,12 +2,17 @@ package lesson8.createwindow;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CreateWindow {
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setBounds(300,100, 800, 600);
+        f.setMinimumSize(new Dimension(800, 600));
+        f.setLocation(300, 100);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
         f.setVisible(true);
     }
 }
