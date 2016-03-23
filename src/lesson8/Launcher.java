46a55305c847c6d4ddc50be4dc7824d6687aa178
styopa89 <styopa.v@gmail.com>
2016-03-23 13:44:27 +0200

    Java

--- src/lesson8/Launcher.java
+++ src/lesson8/Launcher.java
@@ -1,7 +1,37 @@
 package lesson8;
 
-/**
- * Created by vstepyra on 16.03.2016.
- */
+import java.util.*;
+
+import static java.util.Collections.*;
+
 public class Launcher {
+
+    public static void main(String[] args) {
+        List a= new ArrayList<Integer>();
+        a.add(7);
+        a.add(9);
+        a.add(3);
+        a.add(4);
+        a.add(14);
+        a.add(4);
+        a.add(5);
+        a.add(6);
+        a.add(1);
+        a.add(8);
+        a.add(8);
+        a.add(8);
+        a.add(8);
+        a.add(8);
+        a.add(2);
+        a.add(2);
+        a.add(10);
+        a.add(111);
+        System.out.println(a.size());
+        System.out.println(a.toString());
+        Set b = new TreeSet<Integer>();
+        b.addAll(a);
+        System.out.println(b.size());
+        System.out.println(b.toString());
+
+    }
 }
