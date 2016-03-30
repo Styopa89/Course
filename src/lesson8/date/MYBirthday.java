f872927445809fbb8778fb320fcae895ffd1c2a6
styopa89 <styopa.v@gmail.com>
2016-03-30 13:10:58 +0300

    Java

---
+++ src/lesson8/date/MYBirthday.java
@@ -0,0 +1,18 @@
+package lesson8.date;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MYBirthday {
+
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yy");
+        Date birthday = format1.parse("01.05.89");
+//        System.out.println(format1.format(birthday));
+        System.out.println(format.format(birthday));
+    }
+}
