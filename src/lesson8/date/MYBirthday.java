903491616554c26fab412915a3e2ea14527d9be3
styopa89 <styopa.v@gmail.com>
2016-03-30 13:35:04 +0300

    Java

--- src/lesson8/date/MYBirthday.java
+++ src/lesson8/date/MYBirthday.java
@@ -3,7 +3,9 @@ package lesson8.date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MYBirthday {
 
@@ -14,5 +16,9 @@ public class MYBirthday {
         Date birthday = format1.parse("01.05.89");
 //        System.out.println(format1.format(birthday));
         System.out.println(format.format(birthday));
+        Calendar c = new GregorianCalendar();
+        c.setTime(birthday);
+        SimpleDateFormat mount = new SimpleDateFormat("EEE");
+        System.out.println(mount.format(birthday));
     }
 }
---
+++ src/lesson8/date/WorkingWithDates.java
@@ -0,0 +1,13 @@
+package lesson8.date;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class WorkingWithDates {
+    public static void main(String[] args) {
+        Calendar c = new GregorianCalendar();
+        System.out.println(c.getTime());
+        System.out.println(Calendar.APRIL);
+    }
+}
